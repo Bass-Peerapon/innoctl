4d44e956a0de6099a34d8486e8244847dbe3cc96
peerapon.bua <peerapon.bua@innovasive.co.th>
2024-12-26 12:12:46 +0700

    feat: version cmd

---
+++ cmd/version.go
@@ -0,0 +1,58 @@
+package cmd
+
+import (
+	"fmt"
+	"runtime/debug"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+// version is the version of the cli to be overwritten by goreleaser in the CI run with the version of the release in github
+var version string
+
+func getVersion() string {
+	noVersionAvailable := "No version info available for this build, run 'innoctl help version' for additional info"
+
+	if len(version) != 0 {
+		return version
+	}
+
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		return noVersionAvailable
+	}
+
+	// If no main version is available, Go defaults it to (devel)
+	if bi.Main.Version != "(devel)" {
+		return bi.Main.Version
+	}
+
+	var vcsRevision string
+	var vcsTime time.Time
+	for _, setting := range bi.Settings {
+		switch setting.Key {
+		case "vcs.revision":
+			vcsRevision = setting.Value
+		case "vcs.time":
+			vcsTime, _ = time.Parse(time.RFC3339, setting.Value)
+		}
+	}
+
+	if vcsRevision != "" {
+		return fmt.Sprintf("%s, (%s)", vcsRevision, vcsTime)
+	}
+
+	return noVersionAvailable
+}
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Display application version information.",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		version := getVersion()
+		fmt.Printf("Inno CLI version: %v\n", version)
+	},
+}
